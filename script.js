c6d142095cbdebdf276829a07e57ffe9162be475
Capelaum <thecapellett@gmail.com>
2020-05-15 19:25:50 -0300

    Set size and qt  - Modal

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-//* Eetorna o comando para constante!
+//* encapsulate the Selectors comands
 const sel = (el) => document.querySelector(el);
 const all = (el) => document.querySelectorAll(el);
 
-// mapeia as pizzas e realiza listagem
+let modalQt = 1;    
+
+// Pizza maping ant listing
 pizzaJson.map((pizza, index) => {
-    // clona elemento e seu conteudo
+    // clones the pizza item and content
     let pizzaItem = sel('.models .pizza-item').cloneNode(true);
     
-    // identifica a pizza
+    // Generate an identifier for tthe pizza
     pizzaItem.setAttribute('data-key', index);
 
-    // preenche as infos em pizza-item
+    // Pizza itens info 
     pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
@@ -19,11 +21,28 @@ pizzaJson.map((pizza, index) => {
     // Show Modal Event
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+
+        //* Get the pizza key
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        modalQt = 1;    // reset pizza qtd 
 
         sel('.pizzaBig img').src = pizzaJson[key].img;
         sel('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         sel('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        sel('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+        // remove selected size item
+        sel('.pizzaInfo--size.selected').classList.remove('selected');
+        all('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            //* big size selected!
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+        sel('.pizzaInfo--qt').innerHTML = modalQt;
 
         sel('.pizzaWindowArea').style.opacity = 0;
         sel('.pizzaWindowArea').style.display = 'flex';
@@ -44,7 +63,33 @@ function closeModal() {
         sel('.pizzaWindowArea').style.display = 'none';
     }, 500);
 }
-// gera um array com os 2 botoes de cancelar
-all('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((botao) => {
-    botao.addEventListener('click', closeModal);
-})
+
+// Array of 2 Cancel Buttons
+all('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((btn) => {
+    btn.addEventListener('click', closeModal);
+});
+
+// seting the qt buttons
+sel('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQt > 1){
+        modalQt--;
+        sel('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+// max = 50 pizzas
+sel('.pizzaInfo--qtmais').addEventListener('click', () => {
+    if(modalQt < 50){
+        modalQt++;
+        sel('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+// select size
+all('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        // reset
+        sel('.pizzaInfo--size.selected').classList.remove('selected');  
+
+        // add
+        size.classList.add('selected');
+    })
+});
