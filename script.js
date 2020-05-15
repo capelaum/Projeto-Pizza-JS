fea94db0f31068c5ed4194432797c693cdb8747c
Capelaum <thecapellett@gmail.com>
2020-05-15 07:08:26 -0300

    Selectors Constants
    Iniciando function de mapeamento das pizzas para colorar nos elementos HTML

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// mapeia item a item e realiza uma operation
+//* Eetorna o comando para constante!
+const sel = (el) => document.querySelector(el);
+const all = (el) => document.querySelectorAll(el);
+
+// mapeia as pizzas e realiza uma operation
 pizzaJson.map((pizza, index) => {
-    console.log(pizza);
+    // clona o elemento e seu conteudo
+    let pizzaItem = sel('.models .pizza-item').cloneNode(true);
+    // preenche as infos em pizza-item
+
+    sel('.pizza-area').append(pizzaItem);
 })
